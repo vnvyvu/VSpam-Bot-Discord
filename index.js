ca2b04c2e0bdc6b1c7d856de952e457bb7022de5
vyvu <vutrivi99@yahoo.com.vn>
2020-08-30 20:53:58 +0700

    Learning...

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ client.once('ready', ()=>{
 });
 
 client.on('message', (...args) => {
-    if(args.content === `${config.prefix}`){
+    if(args[0].content === `${config.prefix}`){
         msg.channel.send('OMG!That you!');
+    }
+    if(args[0].content ===`${config.prefix}prefix`){
+        config.prefix=args[1].content;
     }
 });
 
